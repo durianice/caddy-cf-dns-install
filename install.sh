53d7483d4bc5bcc1b54e3307dd5c5be640f43055
Cookie <135111234+durianice@users.noreply.github.com>
2024-06-03 22:51:22 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,3 +7,7 @@ chmod +x caddy_linux_amd64_custom
 ./caddy_linux_amd64_custom list-modules | grep dns
 sudo cp caddy_linux_amd64_custom /usr/bin/caddy
 systemctl restart caddy
+
+# diable auto update to avoid auto rm file '/usr/bin/caddy'
+sudo apt-mark hold caddy
+# sudo apt-mark unhold caddy
