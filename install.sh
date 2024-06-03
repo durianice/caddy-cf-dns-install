0a68602941254e64f35e395873eecd8df4c5c4d1
Cookie <135111234+durianice@users.noreply.github.com>
2024-06-03 22:48:22 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+#/bin/bash
+
+cd ~
+wget -O caddy_linux_amd64_custom "https://caddyserver.com/api/download?os=linux&arch=amd64&p=github.com%2Fcaddy-dns%2Fcloudflare&idempotency=72186849443355"
+systemctl stop caddy
+chmod +x caddy_linux_amd64_custom
+./caddy_linux_amd64_custom list-modules | grep dns
+sudo cp caddy_linux_amd64_custom /usr/bin/caddy
+systemctl restart caddy
